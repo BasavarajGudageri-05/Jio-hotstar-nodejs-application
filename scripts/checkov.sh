42aaf0f090ac6b77242c0480482d81e660c357f1
Basavaraj Gudageri <42531978+BasavarajGudageri-05@users.noreply.github.com>
2025-10-31 10:16:13 +0530

    updated src code

--- scripts/checkov.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-sudo apt update
-sudo apt install python3-pip
-sudo apt install python3-venv
-mkdir ~/my_terraform_env
-cd ~/my_terraform_env
-python3 -m venv venv
-source venv/bin/activate
-pip install checkov
